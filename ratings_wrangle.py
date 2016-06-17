d000afcad5aed1fd03f8d033c1c6f8a685240478
Jesse <jcahill2252gmail.com>
2016-06-17 15:44:58 -0400

    Working correctly in limited scope with small test data set using only read/write method

--- ratings_wrangle.py
+++ ratings_wrangle.py
@@ -1,18 +1,25 @@
 filename = "E:\NETS_Clients2013ASCI\NETS2013_Ratings.txt"
 
-with open('temp.csv') as f, open('out.csv', 'w') as f2:
-    f2.write('Key, Year, A, B,\n')
-    for i, line in enumerate(f):
-        if i == 0:
-            strip = line.strip().split(',')
-            _, headers = strip[0], strip[1:]
-            headers = [(h[0], '19' + h[1:]) for h in headers]
-        else:
-            strip = line.strip().split(',')
-            key, rest = strip[0], strip[1:]
-            resta = rest[::2]
-            restb = rest[1::2]
-            for (a, year), valuea,valueb in zip(headers, resta, restb):
-                f2.write(','.join([key, year, valuea, valueb]) + '\n')
+def splitlist(numsplits,list):
+    l = [0]*numsplits
+    for i in xrange(numsplits):
+        l[i] = list[i::numsplits]
+    return l
 
+def w2l(numyrs):
+    with open('temp.txt') as f, open('out.csv', 'w') as f2:
+        f2.write('Key, Year, A, B,\n')
+        for i, line in enumerate(f):
+            if i == 0:
+                strip = line.strip().split('\t')
+                _, headers = strip[0], strip[1:]
+                headers = ['19' + h[1:] for h in headers]
+            else:
+                strip = line.strip().split('\t')
+                key, rest = strip[0], strip[1:]
+                restsplit = splitlist(numyrs, rest) # [rest[i:i+numvars] for i in xrange(0, len(rest),numvars)]#
+                for year, values in zip(headers, restsplit):
+                    f2.write(','.join([key, year, ','.join(values)]) + '\n')
 
+if __name__ == '__main__':
+    w2l(3)
