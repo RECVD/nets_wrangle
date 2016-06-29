025482f7b7d7835470c023709576c4132b7ec46d
Jesse <jcahill2252gmail.com>
2016-06-29 10:26:44 -0400

    Succesfful re-creation of functional program into single class OOP

--- ratings_wrangle.py
+++ ratings_wrangle.py
@@ -2,111 +2,120 @@ from __future__ import print_function
 from itertools import chain
 import time
 
-filename = "C:\Users\jc4673\Documents\Columbia\NETS_Clients2013ASCI\NETS2013_SIC.txt"
+class Reader:
+    def __init__(self, filepath, delim_type, line_limit):
+        self.filepath = filepath
+        self.decades = ('0', '1', '9') #decade in YYYY date format
+        self.line_limit = line_limit
 
-def splitlist(numsplits,l, ratings):
-    """
-    Split List into sub-lists of equal size whilst preserving order
-    ------------
-    Keyword Arguments:
-    numsplits:  number of sublists to return
-    list:  original list to be split
-    ratings: is the file the wonky ordering of 'ratings', very specific to this type
-    """
-    listlen = len(l)
-    if not ratings:
-        for i in range(0, listlen, numsplits):
-            yield l[i:i+numsplits]
+        if delim_type == ',' or delim_type.lower() == 'comma':
+            self.delim_type = ','
+        elif delim_type.lower() == 'tab':
+            self.delim_type = '\t'
+        else: raise TypeError('Invalid delimiter: please enter "tab" or ","\n')
+        self.open_file()
+        self.f2 = open('out.txt', 'w')
 
-    else:
-        inc = listlen/numsplits # num to increment by
-        k=inc
-        for j in range(0, inc):
-            indices = [j, j+k, j+k+1]
-            k = k+1
-            yield [l[j] for j in indices]
+    def open_file(self):
+        self.f = open(self.filepath)
 
-def remove_duplicates(seq):
-    """Order-Preserving method of removing duplicates from an iterable """
-    seen = set()
-    seen_add = seen.add
-    return [x for x in seq if not (x in seen or seen_add(x))]
+    def splitlist(self, numsplits, l, ratings):
+        """
+        Split List into sub-lists of equal size whilst preserving order
+        ------------
+        Keyword Arguments:
+        numsplits:  number of sublists to return
+        list:  original list to be split
+        ratings: is the file the wonky ordering of 'ratings', very specific to this type
+        """
+        listlen = len(l)
+        if not ratings:
+            for i in range(0, listlen, numsplits):
+                yield l[i:i + numsplits]
 
+        else:
+            inc = listlen / numsplits  # num to increment by
+            k = inc
+            for j in range(0, inc):
+                indices = [j, j + k, j + k + 1]
+                k = k + 1
+                yield [l[j] for j in indices]
 
-def w2l(filepath, delim_type, linelimit):
-    """
-    Convert data from wide to long format based on dated variables.
-    It is assumed the last two characters of any dated variable is the last two digits of the
-    year.  This works only for 90<=yr<=2019
-    ----------------
-    Keyword arguments:
-        delim_type:  the string delimiter, either 'tab' or ','
+    def remove_duplicates(self, seq):
+        """Order-Preserving method of removing duplicates from an iterable """
+        seen = set()
+        seen_add = seen.add
+        return [x for x in seq if not (x in seen or seen_add(x))]
 
-    """
-    while True:
-        if delim_type.lower() == 'tab':
-            delim = '\t'
-            break
-        elif delim_type.lower() == ',':
-            delim = ','
-            break
-        # else: print("Please specify 'csv' or 'txt'")
-        #   continue
+    def read_line(self, strip=True):
+        """Read a single line and return as a string"""
+        if strip:
+            return self.f.readline().strip().split(self.delim_type)
+        else:
+            return self.f.readline().split(self.delim_type)
 
-    with open(filepath) as f, open('out.txt', 'w') as f2:
+    def get_line1(self):
+        """Read and format first line and save as instance variable"""
+        self.line1 = self.read_line()
+        self.preyears, self.nonyears = [x for x in self.line1[1:] if x[-2] in self.decades], \
+                             [x for x in self.line1[1:] if x[-2] not in self.decades]
 
-        strip1 = f.readline().strip().split(delim) #first line
+        self.yearvars = self.remove_duplicates([x[:-2] for x in self.preyears])
 
-        decades = ('0', '1', '9') #decade digit in YYYY
+        # compute column names
+        colnames = list(chain.from_iterable([[self.line1[0]], self.yearvars, self.nonyears]))
+        colnames.insert(1, 'Year')  # add 'Year" as second element
 
-        # split into time-based columns and non-time based, don't include key
-        preyears, nonyears = [x for x in strip1[1:] if x[-2] in decades], \
-                             [x for x in strip1[1:] if x[-2] not in decades]
+        return self.delim_type.join(colnames) + '%s \n' % self.delim_type
 
-        yearvars = remove_duplicates([x[:-2] for x in preyears])
+    def set_attributes(self):
+        self.year_indices = [self.line1.index(x) for x in self.preyears]
+        self.nonyear_indices = [self.line1.index(x) for x in self.nonyears]
 
-        #compute column names
-        colnames = list(chain.from_iterable([[strip1[0]], yearvars, nonyears]))
-        colnames.insert(1, 'Year') # add 'Year" as second element
-        f2.write(delim.join(colnames) + '%s \n' % delim)
-
-        year_i = [strip1.index(x) for x in preyears]
-        nonyear_i = [strip1.index(x) for x in nonyears]
-
-        allyears = ['19' + h[-2:] if h[-2] == '9' else '20' + h[-2:] for h in preyears] # convert to 'YYYY' format
-        uyears = remove_duplicates(allyears)
+        allyears = ['19' + h[-2:] if h[-2] == '9' else '20' + h[-2:] for h in self.preyears]  # convert to 'YYYY' format
+        self.unique_years = self.remove_duplicates(allyears)
 
         # Do we need to deal with the funky formatting in the ratings table?
         if len(allyears) > 24 and allyears[0] != allyears[1]:
-            ratings = True
+            self.ratings = True
         else:
-            ratings = False
+            self.ratings = False
 
-        num_all_years = len(allyears) #number of total year-based columns
-        numw2l = num_all_years/len(uyears) #number of vars to go from wide to long
+        self.num_all_years = len(allyears)  # number of total year-based columns
+        self.numw2l = self.num_all_years / len(self.unique_years)  # number of vars to go from wide to long
 
-
-        for _, line in zip(xrange(linelimit), f):
-            strip = line.strip().split(delim)
+    def wide_to_long(self, line_limit):
+        for i in xrange(line_limit):
+            strip = self.read_line()
+            if not strip:  # until there are no lines left
+                break
             try:
-                key, time, nontime = strip[0], [strip[i] for i in year_i], [[strip[i] for i in nonyear_i]]*num_all_years #separate key from the rest of the line
-            except IndexError: #caused by a line having only a key or not enough values, pad it with empty vals
-                padding = ["" for i in range(len(strip1)-len(strip))]
+                key, time, nontime = strip[0], [strip[i] for i in self.year_indices], [
+                    [strip[i] for i in self.nonyear_indices]] * self.num_all_years  # separate key from the rest of the line
+            except IndexError:  # caused by a line having only a key or not enough values, pad it with empty vals
+                padding = ["" for i in range(len(self.line1) - len(strip))]
                 padded = strip + padding
-                key, time, nontime = padded[0], [padded[i] for i in year_i],[[padded[i] for i in nonyear_i]] * num_all_years
+                key, time, nontime = padded[0], [padded[i] for i in self.year_indices], [
+                    [padded[i] for i in self.nonyear_indices]] * self.num_all_years
 
+            timesplit = self.splitlist(self.numw2l, time, self.ratings)  # split into equal lists
 
-            timesplit = splitlist(numw2l, time, ratings) #split into equal lists
-
-            for year, timevals, nontimevals in zip(uyears, timesplit, nontime):
-                if not any(timevals): #if the long to wide values are not empty
+            for year, timevals, nontimevals in zip(self.unique_years, timesplit, nontime):
+                if not any(timevals):  # if the long to wide values are not empty
                     continue
                 else:
-                    f2.write(delim.join([key, year, delim.join(timevals), delim.join(nontimevals)]) + '%s \n' % delim)
+                    self.f2.write(self.delim_type.join([key, year, self.delim_type.join(timevals), self.delim_type.join(nontimevals)]) + '%s \n' % self.delim_type)
 
+    def main(self):
+        line1 = self.get_line1()
+        self.f2.write(line1)
+        self.set_attributes()
+        self.wide_to_long(self.line_limit)
 
 if __name__ == '__main__':
+    filename = "C:\Users\jc4673\Documents\Columbia\NETS_Clients2013ASCI\NETS2013_Sales.txt"
+    a = Reader(filename,'tab', 10**3)
     t0 = time.time()
-    w2l(filename, 'tab', 10**3)
+    a.main()
     t1 = time.time()
     print(t1-t0)
