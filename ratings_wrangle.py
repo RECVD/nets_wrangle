9738623fb4e87b175dea08801b45a103a1804436
Jesse <jcahill2252gmail.com>
2016-07-01 11:19:14 -0400

    Split into multiple classes
    Reader - reads in all data and spits out a generator for each line
    Manipulator - Performs any line manipulations and spits out a generator for the manipulated data
    Writer - writes the data to file

--- ratings_wrangle.py
+++ ratings_wrangle.py
@@ -1,23 +1,39 @@
-from __future__ import print_function
 from itertools import chain
 import time
 
 class Reader:
-    def __init__(self, filepath, delim_type, line_limit):
+    def __init__(self, filepath, delim_type, line_limit=0):
         self.filepath = filepath
-        self.decades = ('0', '1', '9') #decade in YYYY date format
+        self.delim_type = delim_type
         self.line_limit = line_limit
 
-        if delim_type == ',' or delim_type.lower() == 'comma':
-            self.delim_type = ','
-        elif delim_type.lower() == 'tab':
-            self.delim_type = '\t'
-        else: raise TypeError('Invalid delimiter: please enter "tab" or ","\n')
-        self.open_file()
-        self.f2 = open('out.txt', 'w')
+        self.f = self.open_file()
+        self.line_gen = self.read_all()  # create line generator
 
     def open_file(self):
-        self.f = open(self.filepath)
+        """Opens the file specified in 'filepath'"""
+        return open(self.filepath, 'r')
+
+    def read_all(self):
+        """Generator that will yield lines in the file"""
+        if self.line_limit:  # if we're not reading the whole file
+            for _, line in zip(xrange(self.line_limit), self.f):
+                yield line.strip().split(self.delim_type)
+        else:
+            for line in self.f:
+                yield line.strip().split(self.delim_type)
+
+    def close_file(self):
+        self.f.close()
+
+
+class Manipulator:
+    def __init__(self, generator):
+        self.decades = ('0', '1', '9') #decade in YYYY date format
+        self.generator = generator
+
+        self.line1 = self.format_line1(self.set_attributes())
+
 
     def splitlist(self, numsplits, l, ratings):
         """
@@ -42,36 +58,29 @@ class Reader:
                 yield [l[j] for j in indices]
 
     def remove_duplicates(self, seq):
-        """Order-Preserving method of removing duplicates from an iterable """
+        """Efficient order-preserving method of removing duplicates from an iterable """
         seen = set()
         seen_add = seen.add
         return [x for x in seq if not (x in seen or seen_add(x))]
 
-    def read_line(self, strip=True):
-        """Read a single line and return as a string"""
-        if strip:
-            return self.f.readline().strip().split(self.delim_type)
-        else:
-            return self.f.readline().split(self.delim_type)
-
-    def get_line1(self):
-        """Read and format first line and save as instance variable"""
-        self.line1 = self.read_line()
-        self.preyears, self.nonyears = [x for x in self.line1[1:] if x[-2] in self.decades], \
-                             [x for x in self.line1[1:] if x[-2] not in self.decades]
-
-        self.yearvars = self.remove_duplicates([x[:-2] for x in self.preyears])
-
-        # compute column names
-        colnames = list(chain.from_iterable([[self.line1[0]], self.yearvars, self.nonyears]))
+    def format_line1(self, line1):
+        """Read and format first line and return it.  You must pass the first line as an argument.
+        -------------
+        Returns: Formatted First Line.
+        """
+        colnames = list(chain.from_iterable([[line1[0]], self.yearvars, self.nonyears]))
         colnames.insert(1, 'Year')  # add 'Year" as second element
-
-        return self.delim_type.join(colnames) + '%s \n' % self.delim_type
+        return colnames
 
     def set_attributes(self):
-        self.year_indices = [self.line1.index(x) for x in self.preyears]
-        self.nonyear_indices = [self.line1.index(x) for x in self.nonyears]
+        """Set all attributes to be used in later manipulation functions"""
+        line1 = next(self.generator) #get the first line from the generator
 
+        self.preyears, self.nonyears = [x for x in line1[1:] if x[-2] in self.decades], \
+                             [x for x in line1[1:] if x[-2] not in self.decades]
+        self.yearvars = self.remove_duplicates([x[:-2] for x in self.preyears])
+        self.year_indices = [line1.index(x) for x in self.preyears]
+        self.nonyear_indices = [line1.index(x) for x in self.nonyears]
         allyears = ['19' + h[-2:] if h[-2] == '9' else '20' + h[-2:] for h in self.preyears]  # convert to 'YYYY' format
         self.unique_years = self.remove_duplicates(allyears)
 
@@ -84,38 +93,61 @@ class Reader:
         self.num_all_years = len(allyears)  # number of total year-based columns
         self.numw2l = self.num_all_years / len(self.unique_years)  # number of vars to go from wide to long
 
-    def wide_to_long(self, line_limit):
-        for i in xrange(line_limit):
-            strip = self.read_line()
-            if not strip:  # until there are no lines left
-                break
-            try:
-                key, time, nontime = strip[0], [strip[i] for i in self.year_indices], [
-                    [strip[i] for i in self.nonyear_indices]] * self.num_all_years  # separate key from the rest of the line
-            except IndexError:  # caused by a line having only a key or not enough values, pad it with empty vals
-                padding = ["" for i in range(len(self.line1) - len(strip))]
-                padded = strip + padding
-                key, time, nontime = padded[0], [padded[i] for i in self.year_indices], [
-                    [padded[i] for i in self.nonyear_indices]] * self.num_all_years
+        return line1
 
-            timesplit = self.splitlist(self.numw2l, time, self.ratings)  # split into equal lists
+    def wide_to_long_single(self, line):
+        try:
+            key, time, nontime = line[0], [line[i] for i in self.year_indices], [
+                [line[i] for i in self.nonyear_indices]] * self.num_all_years  # separate key from the rest of the line
+        except IndexError:  # caused by a line having only a key or not enough values, pad it with empty vals
+            padding = ["" for i in range(len(self.line1) - len(line))]
+            padded = line + padding
+            key, time, nontime = padded[0], [padded[i] for i in self.year_indices], [
+                [padded[i] for i in self.nonyear_indices]] * self.num_all_years
 
-            for year, timevals, nontimevals in zip(self.unique_years, timesplit, nontime):
-                if not any(timevals):  # if the long to wide values are not empty
-                    continue
-                else:
-                    self.f2.write(self.delim_type.join([key, year, self.delim_type.join(timevals), self.delim_type.join(nontimevals)]) + '%s \n' % self.delim_type)
+        timesplit = self.splitlist(self.numw2l, time, self.ratings)  # split into equal lists
 
-    def main(self):
-        line1 = self.get_line1()
-        self.f2.write(line1)
-        self.set_attributes()
-        self.wide_to_long(self.line_limit)
+        line_list = []
+        for year, timevals, nontimevals in zip(self.unique_years, timesplit, nontime):
+            if not any(timevals):  # if the long to wide values are not empty
+                continue
+            else:
+                line_list.append([key] + [year] + timevals + nontimevals)
+
+        return line_list
+
+    def wide_to_long_all(self):
+        for line in self.generator:
+                line_list = self.wide_to_long_single(line)
+                for single_line in line_list:
+                    yield single_line
+
+
+class Writer:
+    def __init__(self, filepath, delim_type, generator):
+        self.filepath = filepath
+        self.delim_type = delim_type
+        self.generator = generator
+
+        self.f = open(self.filepath, 'w')
+
+    def writeline(self, data_list):
+        self.f.write(self.delim_type.join(data_list) + '%s \n' % self.delim_type)
+
+    def write_all(self ):
+        for line in self.generator:
+            self.writeline(line)
+
 
 if __name__ == '__main__':
-    filename = "C:\Users\jc4673\Documents\Columbia\NETS_Clients2013ASCI\NETS2013_Sales.txt"
-    a = Reader(filename,'tab', 10**3)
     t0 = time.time()
-    a.main()
+    filepath = "C:\Users\jc4673\Documents\Columbia\NETS_Clients2013ASCI\NETS2013_SIC.txt"
+    read = Reader(filepath, '\t', line_limit=10**6)
+    manip = Manipulator(read.line_gen)
+    a = manip.wide_to_long_all()
+    write = Writer('out.txt', '\t', a)
+    write.writeline(manip.line1)
+    write.write_all()
     t1 = time.time()
-    print(t1-t0)
+    print t1-t0
+
