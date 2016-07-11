674f8819701ed1300c58f4d687d2b20528b58051
Jesse <jcahill2252gmail.com>
2016-07-11 16:38:02 -0400

    include test classify_business in json_play

--- json_play.py
+++ json_play.py
@@ -4,5 +4,17 @@ from pprint import pprint
 with open('json_test.json') as f:
     j_test = json.load(f)
 
+def classify_business(business_code, business_code2, business_name):
+    for key, _ in j_test:
+        if (business_code in j_test[key]['business_code'] or
+                    business_code2 in j_test[key]['business_code2'] or
+                    business_name in j_test[key]['business_name']):
+            break
+        else:
+            continue
+    return key
+
+
+
 pprint(j_test)
 
