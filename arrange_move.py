3d7afc10eacb9c1ff5eb69c6a753c8d8ca153069
Jesse <jcahill2252gmail.com>
2016-08-01 15:27:15 -0400

    small update to arrange_move

--- arrange_move.py
+++ arrange_move.py
@@ -1,1 +1,19 @@
-pass
+import wrangle as wr
+
+if __name__ == '__main__':
+    # Define all Filenames
+    company_filename = "C:\Users\jc4673\Documents\Columbia\NETS_Clients2013ASCI\NETS2013_Company.txt"
+    address_first_filename = "C:\Users\jc4673\Documents\Columbia\NETS_Clients2013ASCI\NETS2013_AddressFirst.txt"
+    misc_filename = "C:\Users\jc4673\Documents\Columbia\NETS_Clients2013ASCI\NETS2013_Misc.txt"
+    move_filename = "C:\Users\jc4673\Documents\Columbia\NETS_Clients2013ASCI\NETS2013_Move.txt"
+    delim = '\t'
+
+    # Define writer
+    loc_filepath = "C:\Users\jc4673\Documents\Columbia\NETS_Clients2013ASCI\location_transformed.txt"
+
+    # initialize readers
+    read_company = wr.Reader(company_filename, delim_type=delim, line_limit=20)
+    read_address_first = wr.Reader(address_first_filename, delim_type=delim, line_limit=20)
+    read_misc = wr.Reader(misc_filename, delim_type=delim, line_limit=20)
+    read_move = wr.Reader(move_filename, delim_type=delim, line_limit=20)
+
