5eccbbdbebfd05ad0f61c7f7ab66b66e74e0e1a8
Jesse <jcahill2252gmail.com>
2016-08-16 16:50:36 -0400

    Now works in looping mode for entire file

--- location_wrangle.py
+++ location_wrangle.py
@@ -1,8 +1,8 @@
 import pandas as pd
-import numpy as np
 from time import time
+import itertools as it
 
-
+first = True
 # filenames for all df to be used
 company_filename = "C:\Users\jc4673\Documents\Columbia\NETS_Clients2013ASCI\NETS2013_Company.txt"
 address_first_filename = "C:\Users\jc4673\Documents\Columbia\NETS_Clients2013ASCI\NETS2013_AddressFirst.txt"
@@ -11,14 +11,14 @@ move_filename = "C:\Users\jc4673\Documents\Columbia\NETS_Clients2013ASCI\Move_so
 
 company_df = pd.read_table(company_filename, index_col=['DunsNumber'],
                            usecols=['DunsNumber','Company', 'Address', 'City', 'State', 'ZipCode'],
-                           chunksize=10**3)
+                           chunksize=10**1)
 address_first_df = pd.read_table(address_first_filename, index_col=['DunsNumber'],
                                  usecols=['DunsNumber', 'Address_First', 'City_First', 'CityCode_First',
                                           'State_First', 'ZipCode_First', 'FipsCounty_First'],
-                                 chunksize=10**3)
+                                 chunksize=10**1)
 misc_df = pd.read_table(misc_filename, index_col=['DunsNumber'],
                         usecols=['DunsNumber','FirstYear', 'LastYear', 'Latitude', 'Longitude', 'LevelCode',
-                                 'CityCode', 'FipsCounty'], chunksize=10**3)
+                                 'CityCode', 'FipsCounty'], chunksize=10**1)
 
 move_df = pd.read_table(move_filename, index_col=['DunsNumber', 'MoveYear'],
                         usecols=['DunsNumber', 'MoveYear', 
@@ -26,86 +26,62 @@ move_df = pd.read_table(move_filename, index_col=['DunsNumber', 'MoveYear'],
                                  'DestAddress', 'DestCity', 'DestState', 'DestZIP', 'DestFIPSCounty',
                                  'OriginLatitude', 'OriginLongitude', 'OriginLevelCode'])
 
+for company_chunk, add_first_chunk, misc_chunk in it.izip(company_df, address_first_df, misc_df):
 
-t0=time()
+    company_chunk = company_chunk.join(misc_chunk[['LastYear', 'Latitude', 'Longitude', 'LevelCode', 'FipsCounty']])
+    company_chunk.rename(columns={'Address': 'PrimAdd',
+                                  'LastYear': 'FirstYear',
+                                  'City': 'PrimCity',
+                                  'State': 'PrimState',
+                                  'ZipCode': 'PrimZip',
+                                  'FipsCounty': 'PrimFipsCounty'}, inplace=True)
 
-company_chunk = company_df.get_chunk()
-add_first_chunk = address_first_df.get_chunk()
-misc_chunk = misc_df.get_chunk()
+    add_first_chunk = add_first_chunk.join(misc_chunk['FirstYear'])
+    add_first_chunk = add_first_chunk.set_index('FirstYear', append=True)
+    add_first_chunk.rename(columns={'Address_First':'SecAdd',
+                                    'City_First': 'SecCity',
+                                    'State_First': 'SecState',
+                                    'ZipCode_First': 'SecZip',
+                                    'FipsCounty_First' :'SecFipsCounty'}, inplace=True)
 
+    company_first = company_chunk.index[0]
+    company_last = company_chunk.index[-1]
+    move_chunk = move_df.loc[company_first:company_last, :]
+    move_chunk.index.names = ['DunsNumber', 'FirstYear']
+    move_chunk.rename(columns={'OriginAddress':'PrimAdd', 'DestAddress':'SecAdd',
+                               'OriginCity': 'PrimCity', 'DestCity': 'SecCity',
+                               'OriginState': 'PrimState', 'DestState': 'SecState',
+                               'OriginZIP': 'PrimZip', 'DestZIP': 'SecZip',
+                               'OriginLatitude': 'Latitude', 'OriginLongitude': 'Longitude', 'OriginLevelCode': 'LevelCode',
+                               'OriginFIPSCounty': 'PrimFipsCounty', 'DestFIPSCounty': 'SecFipsCounty'}, inplace=True)
 
+    sec_add = pd.concat([move_chunk[['SecAdd', 'SecCity', 'SecState', 'SecZip', 'SecFipsCounty']], add_first_chunk]).sort_index()
 
+    company_chunk.set_index('FirstYear', append=True, inplace=True)
 
-company_chunk = company_chunk.join(misc_chunk[['LastYear', 'Latitude', 'Longitude', 'LevelCode', 'FipsCounty']])
-company_chunk
-company_chunk.rename(columns={'Address': 'PrimAdd', 
-                              'LastYear': 'FirstYear',
-                              'City': 'PrimCity',
-                              'State': 'PrimState',
-                              'ZipCode': 'PrimZip',
-                              'FipsCounty': 'PrimFipsCounty'}, inplace=True)
+    primadd = pd.concat([move_chunk[['PrimAdd', 'PrimCity', 'PrimState', 'PrimZip', 'PrimFipsCounty', 'Latitude', 'Longitude',
+                                     'LevelCode']], company_chunk]).sort_index()
 
+    prim_add = primadd.reset_index(drop=False) #was True
 
+    prim_add.index = sec_add.index.copy()
 
+    joined = prim_add.join(sec_add, how='outer')
+    joined['Company'].bfill(inplace=True)
 
-add_first_chunk = add_first_chunk.join(misc_chunk['FirstYear'])
-add_first_chunk = add_first_chunk.set_index('FirstYear', append=True)
-add_first_chunk.rename(columns={'Address_First':'SecAdd',
-                                'City_First': 'SecCity',
-                                'State_First': 'SecState',
-                                'ZipCode_First': 'SecZip',
-                                'FipsCounty_First' :'SecFipsCounty'}, inplace=True)
+    del joined['DunsNumber']
 
+    joined.rename(columns={'FirstYear':'LastYear'}, inplace=True)
 
+    column_order = ['LastYear', 'Company', 'PrimAdd', 'PrimCity', 'PrimFipsCounty', 'PrimState', 'PrimZip',
+                    'SecAdd', 'SecCity', 'SecFipsCounty', 'SecState', 'SecZip', 'Latitude', 'Longitude', 'LevelCode']
 
-company_last = company_chunk.index[-1]
-move_chunk = move_df.loc[:company_last,:]
-move_chunk.index.names = ['DunsNumber', 'FirstYear']
-move_chunk.rename(columns={'OriginAddress':'PrimAdd', 'DestAddress':'SecAdd',
-                           'OriginCity': 'PrimCity', 'DestCity': 'SecCity',
-                           'OriginState': 'PrimState', 'DestState': 'SecState',
-                           'OriginZIP': 'PrimZip', 'DestZIP': 'SecZip',
-                           'OriginLatitude': 'Latitude', 'OriginLongitude': 'Longitude', 'OriginLevelCode': 'LevelCode',
-                           'OriginFIPSCounty': 'PrimFipsCounty', 'DestFIPSCounty': 'SecFipsCounty'}, inplace=True)
+    joined = joined[column_order]
 
+    if first:
+        joined.to_csv('NETS2013_LocationsSample.txt', sep='\t')
+        first = False
+    else:
+        joined.to_csv('NETS2013_LocationsSample.txt', sep='\t', mode='a', header=False)
 
-
-sec_add = pd.concat([move_chunk[['SecAdd', 'SecCity', 'SecState', 'SecZip', 'SecFipsCounty']], add_first_chunk]).sort_index()
-
-
-
-company_chunk.set_index('FirstYear', append=True, inplace=True)
-
-
-primadd = pd.concat([move_chunk[['PrimAdd', 'PrimCity', 'PrimState', 'PrimZip', 'PrimFipsCounty', 'Latitude', 'Longitude',
-                                 'LevelCode']], company_chunk]).sort_index()
-
-
-prim_add = primadd.reset_index(drop=False) #was True
-
-
-prim_add.index = sec_add.index.copy()
-
-
-
-joined = prim_add.join(sec_add, how='outer')
-joined['Company'].bfill(inplace=True)
-
-
-del joined['DunsNumber']
-
-
-
-joined.rename(columns={'FirstYear':'LastYear'}, inplace=True)
-
-
-
-column_order = ['LastYear', 'Company', 'PrimAdd', 'PrimCity', 'PrimFipsCounty', 'PrimState', 'PrimZip',
-                'SecAdd', 'SecCity', 'SecFipsCounty', 'SecState', 'SecZip', 'Latitude', 'Longitude', 'LevelCode']
-
-
-
-joined = joined[column_order]
-
-joined.to_csv('NETS2013_LocationsSample.txt', sep='\t')
-print(time() - t0)
+    print('wrote it\n')
