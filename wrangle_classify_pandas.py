c011632eee8df4900bbcffb25119cbda13bf691f
Jesse <jcahill2252gmail.com>
2016-08-30 16:18:03 -0400

    Comments and formatting for wrangle_classify_pandas

--- wrangle_classify_pandas.py
+++ wrangle_classify_pandas.py
@@ -3,6 +3,10 @@ import itertools as it
 import json
 
 class Classifier:
+    """
+    Class used in the classification of businesses based on SIC code, number of employees, annual sales,
+    and company name based on the schema outlined in json_config
+    """
     def __init__(self, config_file, delim = ','):
         """Read JSON config file as global and reformat SIC ranges"""
         self.config_file = config_file
@@ -179,12 +183,20 @@ class Classifier:
 
 
 def make_fullyear(partyear_list, prefix):
+    """Function to redefine 'partial year', i.e '99' into 'full year', i.e '1999'.
+    -------------------
+    Keyword Arguments:
+    partyear_list:  List of column headers, some of which will include partial years
+    prefix:  Prefix that identifies that there will be a partial year following
+    """
     prefix_len = len(prefix)
     fullyear_list = []
     for title in partyear_list:
+        #append as is if the prefix isn't present
         if title[:prefix_len] != prefix:
             fullyear_list.append(title)
         else:
+            #figure out if we'll add '19' or '20', do so and append to the new list
             partyear = title[prefix_len:]
             if partyear[0] == '0' or partyear[0] == '1':
                 partyear = prefix + '20'+ partyear
@@ -197,75 +209,77 @@ def column_replace(column_list, term1, term2):
     """Look for term1 in any elements of column_list and replace with term2 when found"""
     return [item.replace(term1,term2) if term1 in item else item for item in column_list]
 
-
+#define all filenames of interest
 sic_filename = "C:\Users\jc4673\Documents\Columbia\NETS_Clients2013ASCI\NETS2013_SIC.txt"
 sales_filename = "C:\Users\jc4673\Documents\Columbia\NETS_Clients2013ASCI\NETS2013_Sales.txt"
 emp_filename = "C:\Users\jc4673\Documents\Columbia\NETS_Clients2013ASCI\NETS2013_Emp.txt"
 misc_filename = "C:\Users\jc4673\Documents\Columbia\NETS_Clients2013ASCI\NETS2013_Misc.txt"
 company_filename = "C:\Users\jc4673\Documents\Columbia\NETS_Clients2013ASCI\NETS2013_Company.txt"
 
+# create dataframe iterators
 sic_df = pd.read_table(sic_filename, index_col=['DunsNumber'], chunksize=10**2)
 misc_df = pd.read_table(misc_filename, usecols=['DunsNumber', 'FirstYear', 'LastYear'], chunksize=10**2)
 sales_df = pd.read_table(sales_filename, chunksize=10**2)
 emp_df = pd.read_table(emp_filename, chunksize=10**2)
 company_series = pd.read_table(company_filename, usecols=['DunsNumber', 'Company'], index_col=['DunsNumber'], chunksize=10**2)
 
-first=True
+first = True  # Determines whether we write to a new file or append
 for sic_chunk, company_chunk, sales_chunk, emp_chunk, misc_chunk in it.izip(sic_df, company_series, sales_df, emp_df,
-                                                                            misc_df):
+                                                                            misc_df): # Iterate over all dfs
 
-    sic_chunk['Company'] = company_chunk
-    sic_chunk.reset_index(inplace=True)
-    sic_chunk.drop(['SIC2', 'SIC3', 'SIC4', 'SIC6', 'SIC8_2', 'SIC8_3', 'SIC8_4', 'SIC8_5', 'SIC8_6'], axis=1, inplace=True)
-    sic_chunk.rename(columns={'SIC8': '8', 'SICChange':'Change'}, inplace=True)
+    sic_chunk['Company'] = company_chunk #add company name to SIC
+    sic_chunk.reset_index(inplace=True) #remove index for joining
+    sic_chunk.drop(['SIC2', 'SIC3', 'SIC4', 'SIC6', 'SIC8_2', 'SIC8_3', 'SIC8_4', 'SIC8_5', 'SIC8_6'],
+                   axis=1, inplace=True) # We don't need these
+    # remove prefix, to not confuse the long to wide algorithm
+    sic_chunk.rename(columns={'SIC8': 'SICHere', 'SICChange':'Change'}, inplace=True)
+    sic_chunk.columns = make_fullyear(list(sic_chunk.columns), 'SIC')  #Fix years on sic
 
-    sic_chunk.columns = make_fullyear(list(sic_chunk.columns), 'SIC')
-
+    # convert to long, sort, and drop null years
     nowlong = pd.wide_to_long(sic_chunk, ['SIC'], i='DunsNumber', j='year')
-
     nowlong.sort_index(inplace=True)
-
     nowlong.dropna(how='any', inplace=True)
-
     nowlong.drop_duplicates(inplace=True)
     nowlong.index.names = ['DunsNumber', 'FirstYear']
 
-    misc_chunk['FirstYear'] = misc_chunk['FirstYear'] + 1
+    misc_chunk['FirstYear'] = misc_chunk['FirstYear'] + 1 # correct for differences in year
     misc_chunk.set_index(['DunsNumber', 'FirstYear'], inplace=True)
 
-    joined = misc_chunk.join(nowlong, how='outer').ffill()
+    joined = misc_chunk.join(nowlong, how='outer').ffill() #merge in LastYear and fill empty vals forward
 
+    # rectify lastyear column to match actuality
     shift_year = lambda joined: joined.index.get_level_values('FirstYear').to_series().shift(-1)
     lastyear = joined.groupby(level=0).apply(shift_year) \
         .combine_first(joined.LastYear).astype(int) \
         .rename('LastYear')
     joined['LastYear'] = lastyear
 
-
+    #formatting emp
     emp_chunk.columns = column_replace(list(emp_chunk.columns), 'EmpC', 'C')
-
     emp_chunk.rename(columns={'EmpHere': 'Here', 'EmpHereC': 'HereC'}, inplace=True)
     step1 = make_fullyear(list(emp_chunk.columns), 'Emp')
     step2 = make_fullyear(step1, 'C')
     emp_chunk.columns = step2
 
+    # Convert emp to long  and format
     nowlong_emp = pd.wide_to_long(emp_chunk, ['C', 'Emp'], i='DunsNumber', j='year').sort_index().dropna()
     nowlong_emp.index.names = ['DunsNumber', 'FirstYear']
-    nowlong_emp.head()
 
+    #formatting sales
     sales_chunk.columns = column_replace(list(sales_chunk.columns), 'SalesC', 'C')
-
     sales_chunk.rename(columns={'SalesHere': 'Here', 'SalesHereC': 'HereC', 'SalesGrowth': 'Growth', 'SalesGrowthPeer': 'GrowthPeer'}, inplace=True)
     step1 = make_fullyear(list(sales_chunk.columns), 'Sales')
     step2 = make_fullyear(step1, 'C')
     sales_chunk.columns = step2
 
+    #convert sales to long and format
     nowlong_sales = pd.wide_to_long(sales_chunk, ['C', 'Sales'], i='DunsNumber', j='year').sort_index().dropna()
     nowlong_sales.index.names = ['DunsNumber', 'FirstYear']
 
+    #join sales, emp and sic
     final = joined.join(nowlong_sales['Sales'], how='left').join(nowlong_emp['Emp'], how='left')
 
-
+    #apply classifications to each row as a new column
     classy = Classifier('C:/Users/jc4673/Documents/Columbia/nets_wrangle/json_config.json')
     final['Class'] = final.apply(classy.classify, axis=1)
 
